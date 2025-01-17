29468b0d4200b44f8b472bc13e297098bebbf4aa
Yash Yadav <yashyadav88690@gmail.com>
2025-01-17 12:49:04 +0530

    completed start ,stop reset functions

--- script.js
+++ script.js
@@ -3,15 +3,33 @@ let timer=null;
 let startTime = 0;
 let elapsedTime=0;
 let resetTime=0;
-
+let isRunning=false;
 function start(){
-
+if(isRunning){
+    startTime=Date.now()-elapsedTime;
+    timer=setInterval(update,10);
+    elapsedTime=
+    isRunning=true;
+}
 }
 
 function stop(){
-
+if(isRunning){
+    clearInterval(timer);
+    isRunning=false;
+}
 }
 
-function stop(){
+function reset(){
+    timer=null;
+    startTime = 0;
+    elapsedTime=0;
+    resetTime=0;
+    isRunning=false;
 
+}
+function update(){
+    const currentTime=Date.now();
+    elapsedTime=currentTime - startTime;
+    let hours = elapsedTime/(1000*60*60);
 }
