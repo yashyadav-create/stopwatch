b4d99f690e86b355f218994fce5c2d693b7c794a
Yash Yadav <yashyadav88690@gmail.com>
2025-01-17 13:03:19 +0530

    completed with all functionalities

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ let elapsedTime=0;
 let resetTime=0;
 let isRunning=false;
 function start(){
-if(isRunning){
+if(!isRunning){
     startTime=Date.now()-elapsedTime;
     timer=setInterval(update,10);
-    elapsedTime=
     isRunning=true;
+    console.log(startTime)
 }
 }
 
@@ -26,10 +26,19 @@ function reset(){
     elapsedTime=0;
     resetTime=0;
     isRunning=false;
+    display.textContent= `00:00:00:00`
 
 }
 function update(){
     const currentTime=Date.now();
     elapsedTime=currentTime - startTime;
-    let hours = elapsedTime/(1000*60*60);
+    let hours = Math.floor(elapsedTime/(1000*60*60));
+    let minutes = Math.floor(elapsedTime/(1000*60)%60);
+    let seconds = Math.floor(elapsedTime/1000%60);
+    let miliseconds = Math.floor(elapsedTime%1000/10);
+    hours=hours.toString().padStart(2,"0")
+    minutes=minutes.toString().padStart(2,"0")
+    seconds=seconds.toString().padStart(2,"0")
+    miliseconds=miliseconds.toString().padStart(2,"0")
+    display.textContent=`${hours}:${minutes}:${seconds}:${miliseconds}`
 }
