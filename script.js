5c7f22c94a5494ee78f31c1e139ef227c5b95eee
Yash Yadav <yashyadav88690@gmail.com>
2025-01-17 10:26:05 +0530

    added functions which will support the functionality of start stop and reset of timer watch

--- script.js
+++ script.js
@@ -2,3 +2,16 @@ let display = document.getElementById("display")
 let timer=null;
 let startTime = 0;
 let elapsedTime=0;
+let resetTime=0;
+
+function start(){
+
+}
+
+function stop(){
+
+}
+
+function stop(){
+
+}
